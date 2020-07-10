e1383a6e0240cc0ca0c8f699e64b3569ec359089
hanumanth <hanumanthmaddula@gmail.com>
2020-07-10 16:07:44 +0530

    If example created

---
+++ src/com/bluedart/booking/Ifdemo.java
@@ -0,0 +1,19 @@
+package com.bluedart.booking;
+
+public class Ifdemo {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		if(a>b) {
+			System.out.println("A is greater than B");
+		}
+		if(a<b) {
+			System.out.println("A is less  than B");
+		}
+		if(a==b) {
+			System.out.println("A is equal to B");
+		}
+	}
+
+}
